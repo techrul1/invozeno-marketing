d6af6535e2fcd959ab86ca7ffb65cd931ca6fa2b
techrul1 <techrul1@gmail.com>
2026-06-21 16:32:31 +0100

    Fix missing dependencies and add public assets

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  // Pass-through fetch handler to satisfy Chrome PWA install criteria
+  event.respondWith(fetch(event.request));
+});
